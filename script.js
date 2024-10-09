d1ee4640b82ce5598f571a6522d69ee086368e78
ArijanBrkic <arijanbrkic1@gmail.com>
2024-10-09 23:16:31 +0200

    Added buttons

--- script.js
+++ script.js
@@ -1,14 +1,6 @@
-// script.js
+//We need to create a 16 x 16 BY DEFAULT grid
+//for a bonus challenge, we will make the size be able to change from 4x4 up to 64x64
+//Three settings: one is user picked color, one is rainbow, one is "Shading by 10%"
 
-document.addEventListener("DOMContentLoaded", function() {
-    var container = document.querySelector(".container");
-  
-    for (let i = 0; i < 16; i++) {
-      for (let j = 0; j < 16; j++) {
-        var gridItem = document.createElement("div");
-        gridItem.className = "grid-item";
-        container.appendChild(gridItem);
-      }
-    }
-  });
-  
+const DEFAULT_SIZE = 16;
+const DEFAULT_COLOR = "#000000";
