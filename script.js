ed17fed2a35ca9125c481243680f1a51094d2e49
ArijanBrkic <arijanbrkic1@gmail.com>
2024-10-10 22:16:50 +0200

    Added function to change mode between buttons and added event listeners to change div colors

--- script.js
+++ script.js
@@ -1,8 +1,15 @@
 //We need to create a 16 x 16 BY DEFAULT grid
 //for a bonus challenge, we will make the size be able to change from 4x4 up to 64x64
 //Three settings: one is user picked color, one is rainbow, one is "Shading by 10%"
-const slider = document.getElementById("sizePicker");
+const slider = document.getElementById("slider");
 const container = document.getElementById("container");
+const colorPicker = document.getElementById("colorPicker");
+const rainbowBtn = document.getElementById("rainbowBtn");
+const eraserBtn = document.getElementById("eraserBtn");
+const shadeBtn = document.getElementById("shadeBtn");
+
+let currentMode = 'color';
+let currentColor = '#000000';
 
 //Function to create grid
 function createGrid(size) {
@@ -15,6 +22,7 @@ function createGrid(size) {
     const gridDiv = document.createElement("div");
     gridDiv.classList.add("grid-item");
     container.appendChild(gridDiv);
+    gridDiv.addEventListener('mouseover', changeMode);
 
   }
 
@@ -28,4 +36,25 @@ slider.addEventListener('change', function() {
   const currentSize = slider.value;
   createGrid(currentSize);
   
+});
+
+//we need a function to alternate between the color modes and add event listeners to each button.
+
+function changeMode(e) {
+  if (currentMode === 'color') {
+    e.target.style.backgroundColor = currentColor;
+  } else if (currentMode === 'eraser') {
+    e.target.style.backgroundColor = "#FFFFFF";
+  }
+}
+
+//event listeners for each mode
+
+colorPicker.addEventListener('input', function() {
+  currentMode = 'color';
+  currentColor = colorPicker.value;
+});
+
+eraserBtn.addEventListener('click', function() {
+  currentMode = 'eraser';
 });
